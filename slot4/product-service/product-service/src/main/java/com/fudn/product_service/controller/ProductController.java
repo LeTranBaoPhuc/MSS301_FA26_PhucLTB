3d4ad4e58b05c61191678a251fdc3164cb1b443e
LeTranBaoPhuc <letranbaophucxhb@gmail.com>
2026-09-18 15:58:52 +0700

    feat (TODO3):  implement update product endpoint

--- slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ slot4/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -51,7 +51,12 @@ public class ProductController {
     //          @PathVariable String id,
     //          @RequestBody ProductRequest productRequest) { ... }
     // ==========================================================
-    // TODO: viết endpoint update tại đây
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(
+            @PathVariable String id,
+            @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
 
 
     // ==========================================================
